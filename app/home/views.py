f8615b5665dfe58e64b8b0ab5c4818447ecba43e
jjyyxx213 <114245725@qq.com>
2018-12-03 16:41:27 +0800

    qrcode

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,19 @@
 # -*- coding:utf-8 -*-
 from . import home
-from flask import render_template
+from flask import render_template, request
+from qrcode import QRCode, constants
 
 @home.route('/', methods=['GET'])
 def index():
     return render_template("home/index.html")
+
+@home.route('/qrcode/add', methods=['GET'])
+def add_qrcode():
+    data = request.args.get('data', '')
+    qr = QRCode(
+        version=1,
+        error_correction=constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
